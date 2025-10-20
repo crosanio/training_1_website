30fdc8d1720a9414a8cf9b8cf1455e45bf6f0aba
crosanio <crosanio@8west.ie>
2025-10-20 15:03:28 +0200

    layout.jsx | <head> metadata application

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,13 +1,29 @@
-import './globals.css'
+// UTILITY
+import "./globals.css";
+import { Poppins } from 'next/font/google';
+
+const poppins = Poppins({
+    subsets: ['latin'],
+    weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+    style: ['normal', 'italic'],
+    display: 'swap',
+});
 
 export const metadata = {
-    title: 'Training Website',
-    description: 'A modern Next.js website',
+    metaTitle: 'My Application',
+    metaDescription: 'A modern Next.js website',
 }
 
 export default function RootLayout({ children }) {
     return (
-        <html lang="en">
+        <html lang="en" className={poppins.className}>
+            <head>
+                <meta charSet="UTF-8" />
+                <link rel="icon" type="image/svg+xml" href="/favicon.webp" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <title>{metaTitle}</title>
+                <meta name="description" content={metaDescription}></meta>
+            </head>
             <body>
                 {children}
             </body>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,4 @@
 import styles from './page.module.css';
-import Link from 'next/link';
 import ComponentTemplate from "../../components/ComponentTemplate/ComponentTemplate"
 
 
@@ -8,40 +7,6 @@ export default function HomePage() {
     return (
         <main>
             <ComponentTemplate />
-
-            <div className={styles.navigationLinks}>
-                <Link href="/products">
-                    <button>NAVIGATE - Products</button>
-                </Link>
-
-                <Link href="/products/product-details">
-                    <button>NAVIGATE - Product Details</button>
-                </Link>
-
-                <Link href="/about-us">
-                    <button>NAVIGATE - About Us</button>
-                </Link>
-
-                <Link href="/gallery">
-                    <button>NAVIGATE - Gallery</button>
-                </Link>
-
-                <Link href="/faq">
-                    <button>NAVIGATE - FAQ</button>
-                </Link>
-
-                <Link href="/contacts">
-                    <button>NAVIGATE - Contacts</button>
-                </Link>
-
-                <Link href="/privacy-policy">
-                    <button>NAVIGATE - Privacy Policy</button>
-                </Link>
-
-                <Link href="/not-found">
-                    <button>NAVIGATE - Not Found</button>
-                </Link>
-            </div>
         </main>
     )
 }
